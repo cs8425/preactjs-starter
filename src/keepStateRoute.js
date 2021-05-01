55413927bdb683be4b0e07ee72cc9bb5faf0e536
cs8425 <cs8425@gmail.com>
2021-05-02 02:49:54 +0800

    init some Component & router with keep-alive

---
+++ src/keepStateRoute.js
@@ -0,0 +1,17 @@
+import { h, Component } from 'preact';
+import { useRoute } from 'wouter-preact';
+
+class KeepStateRoute extends Component {
+	render(props, state) {
+		const [isActive, params] = useRoute(props.path);
+		return (isActive) ? h('div', null, props.children) : h('div', { style: 'display: none' }, props.children);
+	}
+}
+// const KeepStateRoute = (props) => {
+// 	const { path, match, component, children } = props;
+// 	const useRouteMatch = useRoute(path);
+// 	const [matches, params] = match || useRouteMatch;
+// 	return (matches) ? h('div', null, children) : h('div', { style: 'display: none' }, children);
+// };
+
+export { KeepStateRoute };
--- src/main.js
+++ src/main.js
@@ -1,18 +1,40 @@
 import { h, Fragment, Component, render } from 'preact';
+import { Link, Route, Router } from 'wouter-preact';
 
-// import { Counter, TodoList } from './comp.js';
-import { Counter, TodoList } from './comp.jsx';
+import { useHashLocation } from './hashRoute.js';
+import { KeepStateRoute } from './keepStateRoute.js';
+
+import { Counter, TodoList } from './comp.js';
+// import { Counter, TodoList } from './comp.jsx';
 
 // Create your app
-const app = h(Fragment, null, [
+const app = h(Router, { hook: useHashLocation }, [
 	h('h1', null, [
 		'Hello World!',
 
 	]),
-	h('div', null, [
+	h('div', { class: 'navlink' }, [
+		h(Link, { href: '/' }, 'all'),
+		h(Link, { href: '/counter' }, 'Counter'),
+		h(Link, { href: '/todolist' }, 'TodoList'),
+	]),
+	h('hr', null),
+
+
+	// h(Route, { path: '/' }, h('div', null, [
+	// 	h(Counter),
+	// 	h(TodoList),
+	// ])),
+	// h(Route, { path: '/counter', component: Counter }),
+	// h(Route, { path: '/todolist', component: TodoList }),
+
+
+	h(KeepStateRoute, { path: '/' }, h('div', null, [
 		h(Counter),
 		h(TodoList),
-	]),
+	])),
+	h(KeepStateRoute, { path: '/counter' }, h(Counter)),
+	h(KeepStateRoute, { path: '/todolist' }, h(TodoList)),
 ]);
 
 render(app, document.getElementById('app'));
