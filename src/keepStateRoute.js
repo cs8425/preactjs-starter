d2cd7f38c43f5b304228fae17fcae0b07b6d03dc
cs8425 <cs8425@gmail.com>
2021-05-02 20:12:43 +0800

    use jsx

--- src/keepStateRoute.js
+++ src/keepStateRoute.js
@@ -1,12 +1,15 @@
 import { h, Component } from 'preact';
 import { useRoute } from 'wouter-preact';
 
+// class style
 class KeepStateRoute extends Component {
 	render(props, state) {
 		const [isActive, params] = useRoute(props.path);
 		return (isActive) ? h('div', null, props.children) : h('div', { style: 'display: none' }, props.children);
 	}
 }
+
+// hook style
 // const KeepStateRoute = (props) => {
 // 	const { path, match, component, children } = props;
 // 	const useRouteMatch = useRoute(path);
--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 import { h, Fragment, Component, render } from 'preact';
 import { Link, Route, Router } from 'wouter-preact';
 
+// router的功能強化(hash URL & 簡易版組件狀態保持)
 import { useHashLocation } from './hashRoute.js';
 import { KeepStateRoute } from './keepStateRoute.js';
 
-import { Counter, TodoList } from './comp.js';
-// import { Counter, TodoList } from './comp.jsx';
+// 引入組件
+// import { Counter, TodoList } from './comp.js';
+import { Counter, TodoList } from './comp.jsx';
 
 // Create your app
-const app = h(Router, { hook: useHashLocation }, [
+/*const app = h(Router, { hook: useHashLocation }, [
 	h('h1', null, [
 		'Hello World!',
-
 	]),
 	h('div', { class: 'navlink' }, [
 		h(Link, { href: '/' }, 'all'),
@@ -35,6 +36,29 @@ const app = h(Router, { hook: useHashLocation }, [
 	])),
 	h(KeepStateRoute, { path: '/counter' }, h(Counter)),
 	h(KeepStateRoute, { path: '/todolist' }, h(TodoList)),
-]);
+]);*/
+
+// jsx
+const app = (
+	<Router hook={useHashLocation}>
+		<h1>Hello World!</h1>
+		<div class="navlink">
+			<Link href="/">all</Link>
+			<Link href="/counter">Counter</Link>
+			<Link href="/todolist">TodoList</Link>
+		</div>
+		<hr/>
+		<KeepStateRoute path="/">
+			<Counter></Counter>
+			<TodoList></TodoList>
+		</KeepStateRoute>
+		<KeepStateRoute path="/counter">
+			<Counter></Counter>
+		</KeepStateRoute>
+		<KeepStateRoute path="/todolist">
+			<TodoList></TodoList>
+		</KeepStateRoute>
+	</Router>
+);
 
 render(app, document.getElementById('app'));
