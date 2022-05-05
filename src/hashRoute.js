ae39b2dcff8a155c5b021cde4ec1250149b4725d
cs8425 <cs8425@gmail.com>
2022-05-05 09:48:50 +0800

    fix hashRoute not update if hash change between render and useEffect

--- src/hashRoute.js
+++ src/hashRoute.js
@@ -1,17 +1,24 @@
-import { useState, useEffect, useCallback } from 'preact/hooks';
+import { useState, useEffect, useRef } from 'preact/hooks';
 
 // returns the current hash location in a normalized form
 // (excluding the leading '#' symbol)
-const currentLoc = () => window.location.hash.replace("#", "") || "/";
+const currentLoc = () => window.location.hash.replace(/^#/, '') || '/';
 
 const navigate = (to) => (window.location.hash = to);
 
 const useHashLocation = () => {
 	const [loc, setLoc] = useState(currentLoc());
+	const prevHash = useRef(currentLoc());
 
 	useEffect(() => {
 		// this function is called whenever the hash changes
 		const handler = () => setLoc(currentLoc());
+
+		// fix if an update has occurred between render and the effect handler
+		if (prevHash.current !== currentLoc()) {
+			prevHash.current = currentLoc();
+			handler();
+		}
 
 		// subscribe to hash changes
 		window.addEventListener("hashchange", handler);
